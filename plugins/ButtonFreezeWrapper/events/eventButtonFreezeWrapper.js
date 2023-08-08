09983b9763e8a2a6017cf8f1da65cd5917bd86db
Plep-m <menut34@gmail.com>
2023-08-08 19:00:08 +0200

    very custom plugin handeling nonmodal closable text

--- plugins/ButtonFreezeWrapper/events/eventButtonFreezeWrapper.js
+++ plugins/ButtonFreezeWrapper/events/eventButtonFreezeWrapper.js
@@ -1,6 +1,6 @@
-const id = "UTGB_EVENT_NON_FREEZING_TEXT";
-const groups = ["Undertale GB", "EVENT_GROUP_DIALOGUE"];
-const name = "Non Freezing Text";
+const id = "UTGB_EVENT_BUTTON_FREEZE_WRAPPER";
+const groups = ["Undertale GB", "EVENT_GROUP_MISC"];
+const name = "Button Freeze Wrapper";
 
 const wrap8Bit = (val) => (256 + (val % 256)) % 256;
 const decOct = (dec) => wrap8Bit(dec).toString(8).padStart(3, "0");
@@ -13,8 +13,15 @@ const autoLabel = (fetchArg) => {
 const fields = [
   {
     key: "variable",
-    label: "Reading state",
+    label: "Freeze state",
     type: "variable",
+  },
+  {
+    key: "vmunlock",
+    label: "VM Unlock ?",
+    type: "checkbox",
+    defaultValue: "true",
+    width: "50%",
   },
   {
     key: "event",
@@ -23,7 +30,7 @@ const fields = [
 ];
 
 const compile = (input, helpers) => {
-  const {_ifVariableConst, _compilePath, _jump, variableSetToValue, getNextLabel, _label, inputScriptSet, inputScriptRemove} = helpers;
+  const {_ifVariableConst, _compilePath, _jump, variableSetToValue, getNextLabel, _label, inputScriptSet, inputScriptRemove, appendRaw} = helpers;
 
   const trueLabel = getNextLabel();
   const endLabel = getNextLabel();
@@ -32,11 +39,17 @@ const compile = (input, helpers) => {
    // execute the events of the false path
   _jump(endLabel); // jump to the end label (after the true path)
     
-  _label(trueLabel); 
+  _label(trueLabel);
+  if(input.vmunlock) {
+    appendRaw(`VM_UNLOCK`)
+  }
   variableSetToValue(input.variable, 1); // set input variable to 1
   inputScriptSet(["up", "down", "left", "right"], true); // set all input to 0
   _compilePath(input.event); // execute the events of the true path
   inputScriptRemove(["up", "down", "left", "right"]); // remove all input
+  if(input.vmunlock) {
+    appendRaw(`VM_LOCK`)
+  }
   variableSetToValue(input.variable, 0); // set input variable to 0
     
   _label(endLabel);
