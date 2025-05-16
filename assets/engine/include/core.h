686f1651f075cd8d6739263c0f12da3bb757fc77
Plep-m <menut34@gmail.com>
2025-05-16 15:35:10 +0200

    Scipted Room 5

---
+++ assets/engine/include/core.h
@@ -0,0 +1,9 @@
+#ifndef _CORE_H_INCLUDE
+#define _CORE_H_INCLUDE
+
+#include <gbdk/platform.h>
+
+void core_reset(void) BANKED;
+void core_run(void) BANKED;
+
+#endif
---
+++ assets/engine/include/states/logo.h
@@ -0,0 +1,9 @@
+#ifndef STATE_LOGO_H
+#define STATE_LOGO_H
+
+#include <gbdk/platform.h>
+
+void logo_init(void) BANKED;
+void logo_update(void) BANKED;
+
+#endif
